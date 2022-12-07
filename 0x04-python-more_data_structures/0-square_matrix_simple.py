366bde2454a213da43db32319964fff567080866
Salihu Abdul <salihuabdul3@gmail.com>
2022-12-07 13:46:43 +0000

    first commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        transpose = []
-        for elem in row:
-            elem = elem * elem
-            transpose.append(elem)
-        new_matrix.append(transpose)
-    return new_matrix  
+    tmp = []
+    for x in matrix:
+        tmp.append(list(map(lambda x: x**2, x)))
+    return (tmp)  
