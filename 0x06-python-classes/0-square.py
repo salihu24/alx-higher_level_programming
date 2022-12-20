b77438d9d28af879af0afcb0f206e7e2a356a702
Salihu Abdul <salihuabdul3@gmail.com>
2022-12-20 04:18:21 -0800

    corrected task comit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-"""Defines a class Square"""
+
+
+"""Define a class Square."""
 
 
 
@@ -8,6 +10,6 @@
 
 class Square:
 
-    """An empty Square class"""
+    """Represent a square."""
 
     pass
