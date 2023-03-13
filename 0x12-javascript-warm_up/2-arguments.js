947766a64422f4973d4d8de0851bbed6fada0a24
Salihu Abdul <salihuabdul3@gmail.com>
2023-03-13 13:11:35 +0000

    correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+
+const len = process.argv.length;
+
+if (len < 3) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (len === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
