473bf92fa4d81a1ed1680faadd8f71175312c92d
Salihu Abdul <salihuabdul3@gmail.com>
2023-03-14 11:18:11 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+const list = require('./100-data.js').list;
+console.log(list);
+const newList = list.map((x, index) => x * index);
+console.log(newList);
